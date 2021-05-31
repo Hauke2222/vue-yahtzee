989fce537126ceb2310439311393df4fb242b8b9
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2021-05-31 15:43:14 +0200

    added diceroll functionality + score tables

--- src/components/DiceRoll.vue
+++ src/components/DiceRoll.vue
@@ -1,13 +1,55 @@
 <template>
-  
+  <div id="diceRollTemplate">
+    <hr />
+    <div
+      style="font-size: 32px"
+      v-for="(n, index) in rolledNumbers"
+      :key="index"
+    >
+      {{ n.unicode + ": " + n.value }}
+    </div>
+    <br />
+    <button @click="rollDice">Rol dobbelstenen</button>
+  </div>
 </template>
 
 <script>
 export default {
+  name: "DiceRoll",
 
-}
+  props: {},
+
+  methods: {
+    rollDice() {
+      this.resetRolledNumbers();
+      for (let i = 0; i < this.numberOfDices; i++) {
+        this.dices[i] = Math.floor(Math.random() * 6) + 1;
+        this.rolledNumbers[this.dices[i] - 1].value++;
+      }
+    },
+    resetRolledNumbers() {
+      this.rolledNumbers = [
+        { eyes: 1, value: 0, unicode: "\u2680" },
+        { eyes: 2, value: 0, unicode: "\u2681" },
+        { eyes: 3, value: 0, unicode: "\u2682" },
+        { eyes: 4, value: 0, unicode: "\u2683" },
+        { eyes: 5, value: 0, unicode: "\u2684" },
+        { eyes: 6, value: 0, unicode: "\u2685" },
+      ];
+    },
+  },
+  created() {
+    this.resetRolledNumbers();
+  },
+
+  data() {
+    return {
+      dices: [],
+      numberOfDices: 5,
+      rolledNumbers: [],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/Score.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="scoreTemplate">
+    <br />
+    <table>
+      <thead>
+        <tr>
+          <th>Bovenste helft</th>
+          <th>Punten Telling</th>
+          <th>Spel 1</th>
+        </tr>
+        <tr>
+          <td>Enen</td>
+          <td>Totaal alle enen</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Tweeën</td>
+          <td>Totaal alle tweeën</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Drieën</td>
+          <td>Totaal alle drieën</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Vieren</td>
+          <td>Totaal alle vieren</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Vijven</td>
+          <td>Totaal alle vijven</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Zessen</td>
+          <td>Totaal alle zessen</td>
+          <td></td>
+        </tr>
+      </thead>
+    </table>
+    <table>
+      <thead>
+        <tr>
+          <th>Onderste helft</th>
+          <th>Punten Telling</th>
+          <th>Spel 1</th>
+        </tr>
+        <tr>
+          <td>3 of a kind</td>
+          <td>Totaal van alle stenen</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Carré</td>
+          <td>Totaal van alle stenen</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Full House</td>
+          <td>25 punten</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Kleine Straat</td>
+          <td>30 punten</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Grote Straat</td>
+          <td>40 punten</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Yahtzee</td>
+          <td>50 punten</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Chance</td>
+          <td>Totaal 5 stenen</td>
+          <td></td>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Score",
+
+  methods: {},
+};
+</script>
+
+<style>
+#scoreTemplate {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+table {
+  text-align: left;
+  margin: 24px;
+}
+
+th,
+td {
+  border: 1px solid lightgray;
+  text-align: left;
+}
+</style>
