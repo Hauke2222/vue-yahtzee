fcd71f3a209facdddba158168f037f2bd1d0da32
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2021-06-16 12:14:35 +0200

    deleted unused component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Yahtzee scoreblad" />
+    <h1>Yahtzee scoreblad</h1>
     <Score />
     <DiceRoll />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import DiceRoll from "./components/DiceRoll.vue";
 import Score from "./components/Score.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     DiceRoll,
     Score,
   },
